71ad959730e153256d813d4db51f9f96cbb67c36
ledinhthinh91 <130221951+ledinhthinh91@users.noreply.github.com>
2026-02-22 23:19:52 +0700

    Add sets for drawn devices and names

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const drawnDevices = new Set();
+const drawnNames = new Set();
+
 const PORT = process.env.PORT || 3000;
 
 // ===== CẤU HÌNH GIẢI =====
